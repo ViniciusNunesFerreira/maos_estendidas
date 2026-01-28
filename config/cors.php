8d04bf0dd467df4e520c0dfcb03d45e7e5928757
ViniciusNunesFerreira <viniciusnunes_ads@hotmail.com>
2026-01-27 22:31:24 -0300

    add cors api

---
+++ config/cors.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => [
+        'api/*', 
+        'app/*', 
+        'broadcasting/auth', 
+        'sanctum/csrf-cookie',
+        'login',
+        'logout'
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://filhos.paitonayede.com.br',      
+        'https://maosestendidas.paitonayede.com.br', // API (útil para o PDV)
+        'http://localhost:3000',             
+        'http://localhost:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => [
+        'Content-Type', 
+        'X-Requested-With', 
+        'Authorization', 
+        'Accept', 
+        'Origin', 
+        'X-XSRF-TOKEN', // Essencial para Sanctum
+        'X-Socket-Id'    // Essencial para Broadcasting
+    ],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
